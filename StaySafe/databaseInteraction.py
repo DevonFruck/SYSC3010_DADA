6f74a6392016795cd0c46605f3fcc5661410557b
Aksh R <aksh.ravishankar@gmail.com>
2020-11-10 21:58:23 -0700

    [Aksh]feat: changed code to improve functionality and added links to proper channels.

--- StaySafe/databaseInteraction.py
+++ StaySafe/databaseInteraction.py
@@ -4,6 +4,7 @@ from urllib.parse import *
 from pip._vendor import requests
 import json
 import sqlite3
+import time
 
 
 def retrieveDataFromTS():
@@ -17,8 +18,8 @@ def retrieveDataFromTS():
     cursor = conn.cursor()
     
     #URLs for pulling data from ThingSpeak
-    s1URL = "https://api.thingspeak.com/channels/1149760/feeds.json?results=3"
-    s2URL = ""
+    s1URL = "https://api.thingspeak.com/channels/1225774/feeds.json?api_key=QO9WBN28PA1X46X8&results=1&timezone=America/New_York"
+    s2URL = "https://api.thingspeak.com/channels/1225784/feeds.json?api_key=6UIX8KY589EGWK9J&results=1&timezone=America/New_York"
     
     #request data and convert it to a json string
     store1 = requests.get(s1URL).json()
@@ -27,50 +28,58 @@ def retrieveDataFromTS():
     #Begin Store 1 Process
     
     #find values required for database from json string
-    for (i) in range(3):
-        if (store1['feeds'][i]['field1'] != None):
-            Count1 = store1['feeds'][i]['field1']
-        else if (store1['feeds'][i]['field2'] != None)
-            Hum1 = store1['feeds'][i]['field2']
-        else 
-            Temp1 = store1['feeds'][i]['field3']
+    Count1 = store1['feeds'][0]['field1']
+    Temp1 = store1['feeds'][0]['field2']
+    Hum1 = store1['feeds'][0]['field3']
 
     #Find values of recorded date and time 
-    Date1 = store1['feeds']['created_at']
+    Date1 = store1['channel']['created_at']
     Date1 = Date1[0:10]
-    Time1 = store1['feeds']['created_at']
+    Time1 = store1['channel']['created_at']
     Time1 = Time1[12:16]
     
     #Insert recorded values into database
-    cursor.execute('''insert into store1Data values (?,?,?,?,?)''',(Date1, Time1, Count1, Temp1, Hum1))
-
+    cursor.execute('''INSERT INTO Store2Data (date,time,count,temperature,humidity) VALUES (?,?,?,?,?)''',(Date1, Time1, Count1, Temp1, Hum1))
+    conn.commit()
+    print("Values stored")
+    
+    cursor.execute('''SELECT * FROM Store1Data''')
+    for row in cursor:
+        print(row['count'],row['temperature'],row['humidity'],row['date'],row['time'])
+    
+#     print(Count1)
+#     print(Temp1)
+#     print(Hum1)
+#     print(Date1)
+#     print(Time1)
+    
     #Begin Store 2 Process.
     
     #find values required for database from json string
-    for (i) in range(3):
-        if (store2['feeds'][i]['field1'] != None):
-            Count2 = store2['feeds'][i]['field1']
-        else if (store2['feeds'][i]['field2'] != None)
-            Hum2 = store2['feeds'][i]['field2']
-        else 
-            Temp2 = store2['feeds'][i]['field3']
-    
-    #Find values of recorded date and time 
-    Date2 = store2['feeds']['created_at']
-    Date2 = Date2[0:10]
-    Time2 = store2['feeds']['created_at']
-    Time2 = Time2[12:16]
+    Count2 = store2['feeds'][0]['field1']
+    Temp2 = store2['feeds'][0]['field2']
+    Hum2 = store2['feeds'][0]['field3']
     
     #Insert recorded values into database
-    cursor.execute('''insert into store2Data values (?,?,?,?,?)''',(Date1, Time1, Count2, Temp2, Hum2))
+    #Uses Date1 and Time1 to avoid query errors that come up when timestamps do not match exactly
+    cursor.execute('''INSERT INTO Store2Data (date,time,count,temperature,humidity) VALUES (?,?,?,?,?)''',(Date1, Time1, Count2, Temp2, Hum2))
+    conn.commit()
+    print("Values stored")
     
+    cursor.execute('''SELECT * FROM Store2Data''')
+    for row in cursor:
+        print(row['count'],row['temperature'],row['humidity'],row['date'],row['time'])
+#     print(Count2)
+#     print(Temp2)
+#     print(Hum2)
     
+    conn.close()
     
 if __name__ == '__main__':
     
     #Every 5 mins(300 seconds) update the database with the recorded values.
     while(True):
         retrieveDataFromTS()
-        sleep(300)
+        time.sleep(30)
 
 
