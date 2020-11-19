13158f3d9ee37711dbcf73d2768eb9fa64a7ad0e
Aksh R <aksh.ravishankar@gmail.com>
2020-11-18 17:17:59 -0700

    [Aksh]feat: addede error handling and extraneous value testing.

--- StaySafe/databaseInteraction.py
+++ StaySafe/databaseInteraction.py
@@ -21,17 +21,27 @@ def retrieveDataFromTS():
     s1URL = "https://api.thingspeak.com/channels/1225774/feeds.json?api_key=QO9WBN28PA1X46X8&results=1&timezone=America/New_York"
     s2URL = "https://api.thingspeak.com/channels/1225784/feeds.json?api_key=6UIX8KY589EGWK9J&results=1&timezone=America/New_York"
     
-    #request data and convert it to a json string
+    #request data and convert it to a json string - Done at nearly same time to avoid mismatched time stamps. Everything after this is done procedurally 
     store1 = requests.get(s1URL).json()
     store2 = requests.get(s2URL).json()
     
     #Begin Store 1 Process
     
     #find values required for database from json string
-    Count1 = store1['feeds'][0]['field1']
-    Temp1 = store1['feeds'][0]['field2']
-    Hum1 = store1['feeds'][0]['field3']
-
+    Count1 = int(store1['feeds'][0]['field1'])
+    Temp1 = float(store1['feeds'][0]['field2'])
+    Hum1 = float(store1['feeds'][0]['field3'])
+    
+    #confirm values fall into expected range, and are not extranneous 
+    if Count1 < 0:
+        Count1 = 0
+    if Temp1 < -15 or Temp1 > 60:
+        print("Invalid temperature")
+        return False
+    if Hum1 < 0 or Hum1 > 100:
+        print("Invalid humidity")
+        return False
+        
     #Find values of recorded date and time 
     Date1 = store1['feeds'][0]['created_at']
     Date1 = Date1[0:10]
@@ -59,16 +69,31 @@ def retrieveDataFromTS():
     #Begin Store 2 Process.
     
     #find values required for database from json string
-    Count2 = store2['feeds'][0]['field1']
-    Temp2 = store2['feeds'][0]['field2']
-    Hum2 = store2['feeds'][0]['field3']
+    Count2 = int(store2['feeds'][0]['field1'])
+    Temp2 = float(store2['feeds'][0]['field2'])
+    Hum2 = float(store2['feeds'][0]['field3'])
+    
+    #confirm values fall into expected range, and are not extranneous.
+    #If count is negative, record it as 0
+    #If environment values are extranneous, exit funciton with failed status.
+    if Count2 < 0:
+        Count2 = 0
+    if Temp2 < -15 or Temp1 > 60:
+        print("Invalid temperature")
+        return False
+    if Hum2 < 0 or Hum1 > 100:
+        print("Invalid humidity")
+        return False
     
     #Insert recorded values into database
     #Uses Date1 and Time1 to avoid query errors that come up when timestamps do not match exactly. Pull is done at nearly the same time, so this is fine.
     cursor.execute('''INSERT INTO Store2Data (date,time,count,temperature,humidity) VALUES (?,?,?,?,?)''',(Date1, Time1, Count2, Temp2, Hum2))
     conn.commit()
+    #Will be commented out for launch, for demos only
     print("Values stored")
     
+    
+    #Printing values are only for Demo purposes only 
     cursor.execute('''SELECT * FROM Store2Data''')
     for row in cursor:
         print(row['count'],row['temperature'],row['humidity'],row['date'],row['time'])
@@ -77,12 +102,18 @@ def retrieveDataFromTS():
 #     print(Hum2)
     
     conn.close()
+    return True
     
 if __name__ == '__main__':
     
     #Every 5 mins(300 seconds) update the database with the recorded values.
     while(True):
-        retrieveDataFromTS()
-        time.sleep(30)
+        #If the storage to the database worked, wait 5 minutes and then store next data point
+        if retrieveDataFromTS():
+            #Use 30 seconds for sake of demos 
+            time.sleep(30)
+        #If the storage to the database failed, try again for clean values.
+        else:
+            retrieveDataFromTS()
 
 
