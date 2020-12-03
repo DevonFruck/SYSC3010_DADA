1708f11dfc9a41e766ff9ba96db877de28f9841f
Anish Tankala <atankala@outlook.com>
2020-12-02 19:11:51 -0500

    [Anish] chore:cleaning up comments

--- StaySafe/people-counting-opencv/people_counter.py
+++ StaySafe/people-counting-opencv/people_counter.py
@@ -24,36 +24,6 @@ import time
 import dlib
 import cv2
 
-# OFFSET_LEFT = 1
-# OFFSET_TOP = 2
-# 
-# NUMS =[1,1,1,1,0,1,1,0,1,1,0,1,1,1,1,  # 0
-#        0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,  # 1
-#        1,1,1,0,0,1,0,1,0,1,0,0,1,1,1,  # 2
-#        1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,  # 3
-#        1,0,0,1,0,1,1,1,1,0,0,1,0,0,1,  # 4
-#        1,1,1,1,0,0,1,1,1,0,0,1,1,1,1,  # 5
-#        1,1,1,1,0,0,1,1,1,1,0,1,1,1,1,  # 6
-#        1,1,1,0,0,1,0,1,0,1,0,0,1,0,0,  # 7
-#        1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,  # 8
-#        1,1,1,1,0,1,1,1,1,0,0,1,0,0,1]  # 9
-# 
-# # Displays a single digit (0-9)
-# def show_digit(val, xd, yd, r, g, b):
-#   offset = val * 15
-#   for p in range(offset, offset + 15):
-#     xt = p % 3
-#     yt = (p-offset) // 3
-#     senseH.set_pixel(xt+xd, yt+yd, r*NUMS[p], g*NUMS[p], b*NUMS[p])
-# 
-# # Displays a two-digits positive number (0-99)
-# def show_number(val, r, g, b):
-#   abs_val = abs(val)
-#   tens = abs_val // 10
-#   units = abs_val % 10
-#   if (abs_val > 9): show_digit(tens, OFFSET_LEFT, OFFSET_TOP, r, g, b)
-#   show_digit(units, OFFSET_LEFT+4, OFFSET_TOP, r, g, b)
-
 def show_capacity(total):
     green = (0,255,0)
     white = (255,255,255)
@@ -136,7 +106,7 @@ totalFrames = 0
 totalDown = 0
 totalUp = 0
 totalPrev = 0
-totalCapacity = 3
+totalCapacity = 3 #change according to store requirements
 
 # start the frames per second throughput estimator
 fps = FPS().start()
